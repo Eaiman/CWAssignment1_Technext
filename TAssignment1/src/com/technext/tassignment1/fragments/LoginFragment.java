96a54d21d9c4c57b3c7b4c5f6b2b7adad314b1dc
Arifuzzaman Sohel <sohel.technext@gmail.com>
2015-01-11 20:43:25 +0600

    gson added

--- TAssignment1/src/com/technext/tassignment1/fragments/LoginFragment.java
+++ TAssignment1/src/com/technext/tassignment1/fragments/LoginFragment.java
@@ -1,5 +1,40 @@
 package com.technext.tassignment1.fragments;
 
-public class LoginFragment {
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class LoginFragment extends Fragment{
+	
+	public LoginFragment(){
+		
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+	}
+	
+	@Override
+	public void onDetach() {
+		// TODO Auto-generated method stub
+		super.onDetach();
+	}
 }
