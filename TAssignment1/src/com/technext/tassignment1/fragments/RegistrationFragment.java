e20ea2b27f328d4178c5f226e1dedcb57693a6dc
Eaiman Shoshi <coolshoshi.cse@gmail.com>
2015-01-14 01:51:48 +0600

    Fragments Adde

--- TAssignment1/src/com/technext/tassignment1/fragments/RegistrationFragment.java
+++ TAssignment1/src/com/technext/tassignment1/fragments/RegistrationFragment.java
@@ -1,5 +1,61 @@
 package com.technext.tassignment1.fragments;
 
-public class RegistrationFragment {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.technext.tassignment1.MainActivity;
+import com.technext.tassignment1.R;
+
+public class RegistrationFragment extends Fragment{
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	private final static String ARG_SECTION_NUMBER = "section_number";
+	
+	public RegistrationFragment() {
+	}
+	
+	/**
+	 * Returns a new instance of this fragment for the given section number.
+	 */
+	public static RegistrationFragment newInstance(int sectionNumber) {
+		RegistrationFragment fragment = new RegistrationFragment();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_registration, container,
+				false);
+		return rootView;
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		((MainActivity) activity).onSectionAttached(getArguments().getInt(
+				ARG_SECTION_NUMBER));
+	}
+	
+	@Override
+	public void onDetach() {
+		// TODO Auto-generated method stub
+		super.onDetach();
+	}
 }
